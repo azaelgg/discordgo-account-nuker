f2bc4475c609ba15835ddded763cc81be7c06780
Azael <53409082+azael1337@users.noreply.github.com>
2020-08-10 14:55:38 +0200

    Update main.go

--- main.go
+++ main.go
@@ -11,22 +11,54 @@ var (
 	Token             string
 )
 
-func leaveAllGuildsWorker(Session *discordgo.Session) {
+func getAllGuildsWorker(Session *discordgo.Session) []string {
+	var guildIDS []string
 	for _, guild := range Session.State.Guilds {
-		err := Session.GuildLeave(guild.ID)
-		if err != nil {
-			fmt.Println("Error leaving server,", err)
-		}
+		guildIDS = append(guildIDS, guild.ID)
 	}
+	return guildIDS
+}
+
+func getAllFriendsWorker(Session *discordgo.Session) []string {
+	var friendIDS []string
+	relationships, err := Session.RelationshipsGet()
+	if err != nil {
+		fmt.Println("[\u001b[31m-\u001b[0m] := Error retrieving friend list,", err)
+	}
+
+	for _, friend := range relationships {
+		friendIDS = append(friendIDS, friend.ID)
+	}
+	return friendIDS
 }
 
 func main() {
+	
 	Session, _ = discordgo.New("")
-	// Session.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36"
+	Session.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36"
 	err := Session.Open()
 	if err != nil {
-		fmt.Println("Error opening connection,", err)
+		fmt.Println("[\u001b[31m-\u001b[0m] := Error opening connection,", err)
 		return
 	}
-	leaveAllGuildsWorker(Session)
+	friendList := getAllFriendsWorker(Session)
+	for _, friend := range friendList {
+		Session.RelationshipDelete(friend)
+		/*
+		if err != nil {
+			fmt.Println("[\u001b[31m-\u001b[0m] := Error removing relationship : " + friend, err)
+		}
+		*/
+		fmt.Println("[\u001b[32m+\u001b[0m] := Relationship removed : " + friend)
+	}
+
+	guildsList := getAllGuildsWorker(Session)
+	for _, guild := range guildsList {
+		err := Session.GuildLeave(guild)
+		if err != nil {
+			fmt.Println("[\u001b[31m-\u001b[0m] := Error leaving guild,", err)
+		}
+		fmt.Println("[\u001b[32m+\u001b[0m] := Guild left : " + guild)
+	}
+
 }
