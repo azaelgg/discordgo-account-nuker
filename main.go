791543c9d7b4a06912b5a47a9a2300b5dbbba1ce
Azael <53409082+azaelgg@users.noreply.github.com>
2021-02-12 11:54:19 +0100

    Update main.go
    recode

--- main.go
+++ main.go
@@ -1,105 +1,89 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "bytes"
-    "bufio"
-    "strings"
-    "net/http"
-    "github.com/bwmarrin/discordgo"	
+	"bufio"
+	"bytes"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
 )
 
-// Vars
-var (
-    Session           *discordgo.Session 
-)
+func getAllGuildsWorker(Session *discordgo.Session) (guildIds []string) {
+	for _, guild := range Session.State.Guilds {
+		guildIds = append(guildIds, guild.ID)
+	}
 
-func getAllGuildsWorker(Session *discordgo.Session) []string {
-    var guildIDS []string
-
-    for _, guild := range Session.State.Guilds {
-        guildIDS = append(guildIDS, guild.ID)
-    }
-
-    return guildIDS
+	return guildIds
 }
 
-func getAllFriendsWorker(Session *discordgo.Session) []string {
-    var friendIDS []string
-    relationships, err := Session.RelationshipsGet()
+func getAllFriendsWorker(Session *discordgo.Session) (friendIDS []string, err error) {
+	if relationships, err := Session.RelationshipsGet(); err == nil {
+		for _, friend := range relationships {
+			friendIDS = append(friendIDS, friend.ID)
+		}
 
-    if err != nil {
-        fmt.Println("[\u001b[31m-\u001b[0m] := Error retrieving friend list,", err)
-    }
+		return friendIDS, nil
+	}
 
-    for _, friend := range relationships {
-        friendIDS = append(friendIDS, friend.ID)
-    }
-
-    return friendIDS
+	return nil, err
 }
 
-func iterateSettingsWorker(Session *discordgo.Session)  {
-    themes := []string{"light", "dark"}
-    locales := []string{"ja", "zh-TW", "ko", "zh-CN"}
-    for _, theme := range themes {
-        for _, locale := range locales {
-            var payload = []byte(fmt.Sprintf(`{"theme": "%v", "locale": "%v"}`, theme, locale))
-            req, err := http.NewRequest("PATCH", "https://discord.com/api/v6/users/@me/settings", bytes.NewBuffer(payload))
-            req.Header.Set("Authorization", Session.Token)
-            req.Header.Set("Content-Type", "application/json")
-        
-            if err != nil {
-                fmt.Println("[\u001b[31m-\u001b[0m] := Error iterating user settings,", err)
-            }
-        
-            client := &http.Client{}
-            _, err = client.Do(req)
-        
-            if err != nil {
-                fmt.Println("[\u001b[31m-\u001b[0m] := Error performing request,", err)
-            }
-        }
-    }
+func iterateSettingsWorker(s *discordgo.Session) {
+	themes := []string{"light", "dark"}
+	locales := []string{"ja", "zh-TW", "ko", "zh-CN"}
+	for _, theme := range themes {
+		for _, locale := range locales {
+			var payload = []byte(fmt.Sprintf(`{"theme": "%v", "locale": "%v"}`, theme, locale))
+			req, err := http.NewRequest("PATCH", "https://discord.com/api/v6/users/@me/settings", bytes.NewBuffer(payload))
+			req.Header.Set("Authorization", s.Token)
+			req.Header.Set("Content-Type", "application/json")
+
+			if err != nil {
+				fmt.Println("[\u001b[31m-\u001b[0m] := Error iterating user settings,", err)
+			}
+
+			client := &http.Client{}
+			_, err = client.Do(req)
+
+			if err != nil {
+				fmt.Println("[\u001b[31m-\u001b[0m] := Error performing request,", err)
+			}
+		}
+	}
 }
 
 func main() {
-    reader := bufio.NewReader(os.Stdin)
-    fmt.Print("[\u001b[32m>\u001b[0m] Enter token : ")
-    token, _ := reader.ReadString('\n')
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("[\u001b[32m>\u001b[0m] Enter token : ")
+	token, _ := reader.ReadString('\n')
 
-    Session, _ = discordgo.New(strings.TrimSpace(token))
-    Session.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36"
-    err := Session.Open()
+	if s, err := discordgo.New(strings.TrimSpace(token)); err == nil {
+		s.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36"
 
-    if err != nil {
-        fmt.Println("[\u001b[31m-\u001b[0m] := Error opening connection,", err)
-        return
-    }
+		friendList, err := getAllFriendsWorker(s)
+		if err != nil {
+			fmt.Println("retrieving friend list:", err)
+		}
+		guildsList := getAllGuildsWorker(s)
 
-    friendList := getAllFriendsWorker(Session)
-    guildsList := getAllGuildsWorker(Session)
+		for _, friend := range friendList {
+			if err := s.RelationshipDelete(friend); err == nil {
+				fmt.Println("[\u001b[32m+\u001b[0m] Relationship removed: " + friend)
+			}
+		}
 
-    for _, friend := range friendList {
-        err := Session.RelationshipDelete(friend)
-        if err != nil {
-            fmt.Println(err)
-        }
-        fmt.Println("[\u001b[32m+\u001b[0m] := Relationship removed : " + friend)
-    }
+		for _, guild := range guildsList {
+			if err := s.GuildLeave(guild); err == nil {
+				fmt.Println("[\u001b[32m+\u001b[0m] Guild left: " + guild)
+			}
+			fmt.Println("[\u001b[31m-\u001b[0m] Error leaving guild,", err)
+		}
 
-    for _, guild := range guildsList {
-        err := Session.GuildLeave(guild)
-        if err != nil {
-            fmt.Println("[\u001b[31m-\u001b[0m] := Error leaving guild,", err)
-        } else {
-            fmt.Println("[\u001b[32m+\u001b[0m] := Guild left : " + guild)
-        }
-    }
-
-    for (true) {
-        iterateSettingsWorker(Session)
-    }
-
+		for true {
+			iterateSettingsWorker(s)
+		}
+	}
 }
